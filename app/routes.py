f7983d31d1ac55c6de77ed592dfdb36048695e21
cptn3m012 <filip.habryn@gmail.com>
2023-10-23 22:42:27 +0200

    creation of basic endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,38 @@
 # Zawiera endpointy dla aplikacji
 # To tutaj definiowane jest, co ma się stać, gdy użytkownik odwiedzi określony adres URL
 from app import app, db
-from flask import render_template
+from flask import render_template, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import User
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user = User(
+        first_name=data['first_name'],
+        last_name=data['last_name'],
+        user_type=data['user_type'],
+        password=hashed_password
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'Registered successfully'}), 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(first_name=data['first_name']).first()
+    if user and check_password_hash(user.password, data['password']):
+              return jsonify({'message': 'Login successful'}), 200
+    return jsonify({'message': 'Invalid credentials'}), 401
 
 
 @app.route('/testdb')
